7284dab899d997abb5c541a0da1aabb83fc1f891
mehmetfiskindal <mehmetfiskindal@gmail.com>
2024-04-14 15:50:57 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/product/constants/product_constants.dart';
+import 'package:myapp/product/theme/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+              fontFamily: ProductConstants.productFontName,
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/product/constants/product_constants.dart
@@ -0,0 +1,4 @@
+final class ProductConstants {
+  ProductConstants._();
+  static String productFontName = 'Roboto';
+}
